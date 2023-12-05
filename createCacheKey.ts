d4f647500327589c58cb399e080ad390b482404f
Matt Pocock <mattpocockvoice@gmail.com>
2023-12-05 10:39:26 +0000

    Fixed cache key

--- createCacheKey.ts
+++ createCacheKey.ts
@@ -1,15 +1,10 @@
 import { V1Input } from "./schema.js";
 
-// Change this whenever you want to manually bust the cache,
-// for instance when changing TS versions
-export const CACHE_BUSTER = "2023-11-29";
-
 export const createCacheKey = (item: V1Input) => {
-  const cacheKey =
-    JSON.stringify({
-      code: item.code,
-      lang: item.lang,
-      meta: item.meta,
-    }) + CACHE_BUSTER;
+  const cacheKey = JSON.stringify({
+    code: item.code,
+    lang: item.lang,
+    meta: item.meta,
+  });
   return cacheKey;
 };
