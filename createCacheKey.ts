0d065406691af16d852a64fe9f9dbeac8d2c8bdf
Matt Pocock <mattpocockvoice@gmail.com>
2023-12-05 10:56:05 +0000

    Added the cache buster back

--- createCacheKey.ts
+++ createCacheKey.ts
@@ -1,10 +1,14 @@
 import { V1Input } from "./schema.js";
 
+// Change this when you want to manually bust the cache
+const CACHE_BUSTER = "v1";
+
 export const createCacheKey = (item: V1Input) => {
   const cacheKey = JSON.stringify({
     code: item.code,
     lang: item.lang,
     meta: item.meta,
+    _key: CACHE_BUSTER,
   });
   return cacheKey;
 };
